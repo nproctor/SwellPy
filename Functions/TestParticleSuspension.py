917e105bf1ea0ed4fc0ca09c651ace6573f7f5b3
Natasha Proctor <nproctor@calpoly.edu>
2016-05-21 14:37:42 -0700

    Add tests for center placement

---
+++ Functions/TestParticleSuspension.py
@@ -0,0 +1,38 @@
+import unittest
+from ParticleSuspension import *
+
+class TestParticleSuspension(unittest.TestCase):
+
+
+	def test_randomCentersShape(self):
+		x = ParticleSuspension(10, 0.2)
+		self.assertEqual(x.centers.shape, (10, 2))
+
+	def test_randomCentersBounds(self):
+		x = ParticleSuspension(10, 0.2)
+		self.assertTrue( (x.centers > 0).all() )
+		self.assertTrue( (x.centers < x.boxsize).all() )
+
+	def test_setBadFormatCenters(self):
+		x = ParticleSuspension(3, 0.2)
+		before = x.centers
+		x.setCenters([0,2,3,4,4,5,6]) # Attempt bad center reassignment (message will print)
+		after = x.centers
+		self.assertTrue( before is after )
+
+	def test_setOutOfBoundsCenters(self):
+		x = ParticleSuspension(2, 0.2)
+		before = x.centers
+		x.setCenters([[0,1],[-10,12]])
+		after = x.centers
+		self.assertTrue( before is after ) # Attemp bad center reassignment (message will print)
+
+	def test_setCenters(self):
+		x = ParticleSuspension(2, 0.2)
+		new = [[0,1],[2.0, 1.89]]
+		x.setCenters(new)
+		self.assertTrue( (x.centers == new).all() )
+ 
+
+if __name__ == "__main__":
+	unittest.main()

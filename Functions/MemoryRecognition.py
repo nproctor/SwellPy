ef3e5472c109a84526758d00787c0db927ae267d
Natasha Proctor <nproctor@calpoly.edu>
2016-05-22 00:55:27 -0700

    Fix typos

--- Functions/MemoryRecognition.py
+++ Functions/MemoryRecognition.py
@@ -1,19 +1,21 @@
+from ParticleSuspension import *
+
 class MemoryRecognition:
     def __init__(self):
         self.system = None
 
     def newSystem(self, N, areaFrac, seed=None):
-        x = ParticleSystem(N, areaFrac, seed)
+        x = ParticleSuspension(N, areaFrac, seed)
         self.system = x
 
     # Query for number of particles at specific swell
-    def numTagAt(self, swell):
+    def fracTagAt(self, swell):
         pairs = self.system.tag(swell)
-        return len(np.unique(pairs)) / self._N
+        return len(np.unique(pairs)) / self.system.N
 
     # Query for number of particles tagged for a range of shears
-    def numTagFor(self, Min, Max, incr):
+    def fracTagFor(self, Min, Max, incr):
         swells = np.arange(Min, Max, incr)
-        tagged = np.array(list(map(lambda x: self.numTagAt(x), swells)))
+        tagged = np.array(list(map(lambda x: self.fracTagAt(x), swells)))
         return tagged
 

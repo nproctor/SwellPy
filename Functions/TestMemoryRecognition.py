beaf7007c87c5c9ac9ee8396920c3d85937ff34e
Natasha Proctor <nproctor@calpoly.edu>
2016-05-22 01:04:04 -0700

    Add test frac tag for

--- Functions/TestMemoryRecognition.py
+++ Functions/TestMemoryRecognition.py
@@ -11,5 +11,11 @@ class TestMemoryRecognition(unittest.TestCase):
 		self.assertEqual(x.fracTagAt(1.0), 1.0)
 		self.assertEqual(x.fracTagAt(0.24), 0)
 
+	def test_fracTagFor(self):
+		x = MemoryRecognition()
+		x.newSystem(3, 0.2)
+		x.system.setCenters([[0,1],[0, 1.25], [0, 0.25]])
+		self.assertTrue( (x.fracTagFor(0, 1.0, 0.2) == [0, 0, 2.0/3, 2.0/3, 1.0, 1.0]).all() )
+
 if __name__ == "__main__":
 	unittest.main()

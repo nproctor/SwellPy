17edcacf524c51eae1cf12c8bbd9b0df489cb191
Natasha Proctor <nproctor@calpoly.edu>
2016-05-23 12:29:54 -0700

    Merge class with MemoryRecognition

--- Functions/ParticleSuspension.py
+++ Functions/ParticleSuspension.py
@@ -116,3 +116,26 @@ class ParticleSuspension:
             i += 1
         return i
 
+    def fracTagAt(self, swell):
+        pairs = self.tag(swell)
+        return len(np.unique(pairs)) / self.N
+
+    # Query for number of particles tagged for a range of shears
+    def fracTag(self, Min, Max, incr):
+        swells = np.arange(Min, Max + incr, incr)
+        tagged = np.array(list(map(lambda x: self.fracTagAt(x), swells)))
+        return swells, tagged
+
+    
+    def tagRate (self, Min, Max, incr):
+        (ignore, tagged) = self.fracTag(Min-incr/2, Max+incr/2, incr)
+        swells = np.arange(Min, Max, incr)
+        rate = ( tagged[1:] - tagged[:-1] ) / incr
+        return swells, rate
+
+    def tagCurvature(self, Min, Max, incr):
+        (ignore, tagRate) = self.tagRate(Min-incr/2, Max+incr/2, incr)
+        swells = np.arange(Min, Max, incr)
+        curve = ( tagRate[1:] - tagRate[:-1] ) / incr
+        return swells, curve
+

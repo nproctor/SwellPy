018e3d36c7b61056ddfc7cd9b7896990d55f48b5
Natasha Proctor <nproctor@calpoly.edu>
2016-06-09 12:27:28 -0700

    Add test for frac not found

--- Functions/TestAnalysis.py
+++ Functions/TestAnalysis.py
@@ -10,14 +10,27 @@ class TestAnalysis(unittest.TestCase):
 		self.assertEqual(Analysis.searchForFrac(x, 1.0, 0.01), 0.5)
 
 	def test_binarySearch2(self):
+		x = ParticleSuspension(2, 0.2)
+		x.setCenters([[0,0],[0.1, 0]])
+		self.assertEqual(Analysis.searchForFrac(x, 1.0, 0.01), 0.1)
+
+	def test_binarySearch3(self):
 		x = ParticleSuspension(3, 0.2)
 		x.setCenters([[0,0],[0.5, 0], [1.3, 0]])
 		self.assertEqual(Analysis.searchForFrac(x, 1.0, 0.01), 1.3-0.5)
 
-	def test_binarySearch3(self):
+	def test_binarySearch4(self):
 		x = ParticleSuspension(4, 0.2)
 		x.setCenters([[0,0],[0.5, 0], [1.3, 0], [1.3, 1.3]])
 		self.assertEqual(Analysis.searchForFrac(x, 1.0, 0.01), 1.3)
+
+	def test_binarySearch5(self):
+		x = ParticleSuspension(4, 0.2)
+		x.setCenters([[0,0],[0.5, 0], [1.3, 0], [1.3, 1.3]])
+		self.assertEqual(Analysis.searchForFrac(x, 0.20, 0.01), 0.5)
+
+
+
 
 	
 

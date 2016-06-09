17f18989557e73b2fbe84b314cbb53d4e861b236
Natasha Proctor <nproctor@calpoly.edu>
2016-06-09 12:30:02 -0700

    Fix for dissolved class
    The StatisticalRecognition class no longer exists. Changing the
    method calls to functions calls to correct for this.

--- Functions/TestStatisticalRecognition.py
+++ Functions/TestStatisticalRecognition.py
@@ -1,25 +1,23 @@
 import unittest
 import numpy as np
 from ParticleSuspension import *
-from StatisticalRecognition import *
+import StatisticalRecognition as sr
 
 class TestStatisticalRecognition(unittest.TestCase):
 
     def test_load_save(self):
-        x = StatisticalRecognition()
         a = [0,0,0,0,0]
         b = [1,1,1,1,1]
-        x.save("test.txt", [a,b], header="This is a test file")
-        (A, B) = x.load("test.txt")
+        sr.save("test.txt", [a,b], header="This is a test file")
+        (A, B) = sr.load("test.txt")
         self.assertTrue( np.array_equal(A, a) ) 
         self.assertTrue( np.array_equal(B, b) )
 
     # Created the params in genCurveNoiseFit.py
     def test_curveNoiseFit(self):
         x = ParticleSuspension(100, 0.2)
-        (swells, curve) = x.tagCurvature(0, 2.0, 0.01)
+        (swells, curve) = x.tagCurve(0, 2.0, 0.01)
         Max = max(curve)
-        sr = StatisticalRecognition()
         meanParams, sdParams = sr.load("curveNoiseParams.txt")
         mean, sd = sr.expectedNoise(100, meanParams, sdParams)
         self.assertTrue( ((mean - sd*1.5) < Max < (mean + sd*1.5)) )
@@ -28,7 +26,6 @@ class TestStatisticalRecognition(unittest.TestCase):
         x = ParticleSuspension(100, 0.2)
         (swells, rate) = x.tagRate(0, 2.0, 0.01)
         Max = max(rate)
-        sr = StatisticalRecognition()
         meanParams, sdParams = sr.load("rateNoiseParams.txt")
         mean, sd = sr.expectedNoise(100, meanParams, sdParams)
         self.assertTrue( ((mean - sd*1.5) < Max < (mean + sd*1.5)) )

dbf774809a291583a7670517ba093a765a5d2160
Natasha Proctor <nproctor@calpoly.edu>
2017-09-22 18:42:32 -0700

    Clean up

--- Functions/crepel.pyx
+++
@@ -1,9 +0,0 @@
-import numpy as np
-cimport numpy as np
-
-def iterate (np.ndarray[np.float64_t, ndim=2] data, np.ndarray[np.int64_t, ndim=1] idx, np.ndarray[np.float64_t, ndim=3] kick, int size):
-    cdef int i = 0
-    while i < size:
-        data[idx[i], 0] += kick[i, 0, 0]
-        data[idx[i], 1] += kick[i, 0, 1]
-        i += 1
--- Functions/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-  name = 'C Repel Particles App',
-  include_dirs = [np.get_include()],
-  ext_modules = cythonize("crepel.pyx"),
-)

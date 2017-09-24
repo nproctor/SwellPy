9b30ef86a75aca814c54fbf13cb09abeb75c216c
Natasha Proctor <nproctor@calpoly.edu>
2017-09-24 15:08:35 -0700

    Include cython as dependency

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 from setuptools import setup
-from Cython.Build import cythonize
+from setuptools.extension import Extension
+from setuptools.command.build_ext import build_ext
+
+class BuildExt(build_ext):
+    def run(self):
+        import numpy as np
+        self.include_dirs.append(np.get_include())
+        build_ext.run(self)
 
 setup(
     name='SwellPy',
     version='0.1',
-    setup_require=[
-        'cython>=0.26',
-        'nose>=1.0'
-    ],
     install_requires=[
+        'setuptools>=18.0',
+        'cython>=0.26',
         'scipy>=0.18',
         'numpy>=1.11',
         'peakutils>=1.0',
         'matplotlib>=2.0'
     ],
     packages=['swellpy'],
-    ext_module = cythonize("*/*.pyx"),
+    cmdclass={'build_ext': BuildExt},
+    ext_modules = [
+        Extension(
+            "crepel", 
+            sources=["swellpy/crepel.pyx"]
+        )
+    ],
     test_suite='nose.collector',
     tests_require=[
         'nose'

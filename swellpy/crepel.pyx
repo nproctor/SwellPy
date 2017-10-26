940eaf6242df8308d0c68e0b862e40a354500c8a
Natasha Proctor <nproctor@calpoly.edu>
2017-10-26 00:44:11 -0700

    Undo attempt to parallelize repulsion

--- swellpy/crepel.pyx
+++ swellpy/crepel.pyx
@@ -1,9 +1,9 @@
+cimport cython
 import numpy as np
 cimport numpy as np
 
-def iterate (np.ndarray[np.float64_t, ndim=2] data, np.ndarray[np.int64_t, ndim=1] idx, np.ndarray[np.float64_t, ndim=3] kick, int size):
-    cdef int i = 0
-    while i < size:
+def iterate (double[:, :] data, long[:] idx, double[:, :, :] kick, long size):
+    cdef long i, N = size
+    for i in range(N):
         data[idx[i], 0] += kick[i, 0, 0]
         data[idx[i], 1] += kick[i, 0, 1]
-        i += 1
